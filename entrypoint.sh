525766124ab23e42ef4dd18baea751f54bd47547
violine1101 <violine1101@gmail.com>
2024-09-04 14:29:41 -0700

    Attempt logging into azure with all the info

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 set -eu
 
+# Log in to Azure CLI
+AZURE_CLIENT_ID=$INPUT_AZURE_CLIENT_ID
+AZURE_TENANT_ID=$INPUT_AZURE_TENANT_ID
+AZURE_SUBSCRIPTION_ID=$INPUT_AZURE_SUBSCRIPTION_ID
+az login --tenant $AZURE_TENANT_ID --username $AZURE_CLIENT_ID
+
 # Open bastion tunnel
 echo Installing prerequisites...
 # python3 -m pip install --upgrade pip --user
-az login
 az extension add --name bastion
 
 echo Opening tunnel...
-AZURE_PORT=50022
-AZURE_IP=127.0.0.1
+BASTION_PORT=50022
+BASTION_IP=127.0.0.1
 
-az network bastion tunnel --name $INPUT_BASTION_NAME --resource-group $INPUT_RESOURCE_GROUP --target-resource-id $INPUT_RESOURCE_ID --resource-port 22 --port $AZURE_PORT &
+az network bastion tunnel --name $INPUT_BASTION_NAME --resource-group $INPUT_RESOURCE_GROUP --target-resource-id $INPUT_RESOURCE_ID --resource-port 22 --port $BASTION_PORT &
 az network bastion wait --created --name $INPUT_BASTION_NAME --resource-group $INPUT_RESOURCE_GROUP
 echo Azure Bastion tunnel established.
 
@@ -25,13 +30,13 @@ chmod 600 ~/.ssh/id_rsa
 echo Uploading artifacts...
 for ARTIFACT_PATH in "${INPUT_ARTIFACT_PATHS[@]}"; do
     echo Uploading artifact $ARTIFACT_PATH...
-    rsync -avh --delete -e 'ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -p $AZURE_PORT' $GITHUB_WORKSPACE/$ARTIFACT_PATH $INPUT_USERNAME@$AZURE_IP:$INPUT_ARTIFACT_DESTINATION
+    rsync -avh --delete -e 'ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -p $BASTION_PORT' $GITHUB_WORKSPACE/$ARTIFACT_PATH $INPUT_USERNAME@$BASTION_IP:$INPUT_ARTIFACT_DESTINATION
 done
 echo All artifacts have been uploaded.
 
 # Run deploy script
 echo Running internal deploy script...
-ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -p $AZURE_PORT $INPUT_USERNAME@$AZURE_IP << EOF
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -p $BASTION_PORT $INPUT_USERNAME@$BASTION_IP << EOF
     $INPUT_SCRIPT
 EOF
 
